7de9fbb1d35065efe21009cc3a195351666f7ed6
ajulcala <ajulcala@everis.com>
2021-11-18 13:37:29 -0500

    configuration local

--- src/main/java/com/profile/app/ProfilesApplication.java
+++ src/main/java/com/profile/app/ProfilesApplication.java
@@ -3,6 +3,12 @@ package com.profile.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * * My <b>Main</b>.
@@ -12,7 +18,21 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableSwagger2
 public class ProfilesApplication {
+
+  /**
+   * My <b>Swagger</b>.
+   * muestran los metodos con vistas HTML
+   *
+   * @Bean
+   */
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+            .select().apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any()).build();
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(ProfilesApplication.class, args);
